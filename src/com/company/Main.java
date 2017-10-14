5327474a741c5ba58abfb2bc9a6fad024c1de02f
Jessie <jessiemh@ufl.edu>
2017-10-14 15:25:58 -0400

    v3

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,5 @@
 package com.company;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
@@ -6,18 +7,36 @@ public class Main {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
         char[] array = str.toCharArray();
-        for(char c: array){
-            if (Character.isDigit(c)){
-                System.out.println(c + " is a number.");
+        for(char c: array) {
+            if (charType(c) == 'i') {
+            System.out.println("Character type is a number.");
+        }
+           else if(charType(c) == 'l'){
+                System.out.println("Character type is a letter.");
             }
-            else if(Character.isLetter(c)){
-                System.out.println(c + " is a letter.");
+            else if (charType(c) == 'w') {
+                System.out.println("Character type is a whitespace.");
             }
-            else if (Character.isWhitespace(c))
-                System.out.println(c + " is a whitespace.");
-            else{
-                System.out.println(c + " is a symbol.");
+            else {
+                System.out.println("Character type is a symbol.");
             }
-            }
+        }
+    }
+    public static char charType(char cPrime) {
+        char myType;
+        if (Character.isDigit(cPrime)) {
+            myType = 'i';
+            //System.out.println(cPrime + " is a number.");
+        } else if (Character.isLetter(cPrime)) {
+            myType = 'l';
+            //System.out.println(cPrime + " is a letter.");
+        } else if (Character.isWhitespace(cPrime)) {
+            myType = 'w';
+            //System.out.println(cPrime + " is a whitespace.");
+        } else {
+            myType = 's';
+            //System.out.println(cPrime + " is a symbol.");
+        }
+        return myType;
     }
 }
