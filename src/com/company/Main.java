b01508d9733f57b46f66129b65c805b7bf7d2584
Jessie <jessiemh@ufl.edu>
2017-10-14 14:29:39 -0400

    v2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,9 +6,16 @@ public class Main {
         Scanner s = new Scanner(System.in);
         String str = s.nextLine();
         char[] array = str.toCharArray();
-        System.out.print("Content of Array: ");
         for(char c: array){
-            System.out.print(c);
-        }
+            if (Character.isDigit(c)){
+                System.out.println(c + " is a number.");
+            }
+            else if(Character.isLetter(c)){
+                System.out.println(c + " is a letter.");
+            }
+            else{
+                System.out.println(c + " is a symbol.");
+            }
+            }
     }
 }
